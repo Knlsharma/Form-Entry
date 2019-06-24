fb677e1e6a98afa1ae74b3be1ba69924a30716c6
Knlsharma <knlsharma678@gmail.com>
2019-06-24 17:08:54 +0530

    added

---
+++ src/main/java/com/orane/registration/module/RegistrationModuleApplication.java
@@ -0,0 +1,13 @@
+package com.orane.registration.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationModuleApplication.class, args);
+	}
+
+}
